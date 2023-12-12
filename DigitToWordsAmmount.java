ef84dff2c68d605fb77b0c418187459330d407e5
Rajkumar <138861148+UggamRajkumar97@users.noreply.github.com>
2023-12-12 12:16:23 +0530

    Add files via upload

--- DigitToWordsAmmount.java
+++ DigitToWordsAmmount.java
@@ -6,7 +6,7 @@ public class DigitToWordsAmmount {
 
 	public static void main(String[] args) {
 
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("enter number");
 		int ammount = sc.nextInt();
 		int temp = ammount;
@@ -17,71 +17,69 @@ public class DigitToWordsAmmount {
 		String[] tens = { "", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
 
 		int p = 1;
-		while(ammount!=0) {
-			ammount=ammount/10;
-			p=p*10;
+		while (ammount != 0) {
+			ammount = ammount / 10;
+			p = p * 10;
 		}
-		p=p/10;
-		ammount=temp;
+		p = p / 10;
+		ammount = temp;
 
 		while (ammount != 0) {
 			int r = ammount / p;
-			//-----1,00,00,000  10,00,00,000
-			if(p==10000000 || p==100000000) {
-				if(p==100000000) {
-					r = ammount / (p/10);
-					if(r>=11 && r<=19) {
-						words=words+doubleDigit[r%10] + " crore ";
-						p=p/10;
-					}else {
-						words=words+tens[r/10];
-						words=words+singleDigit[r%10] + " crore ";
-						p=p/10;
+			// 1 Crore To 10 Crore
+			if (p == 10000000 || p == 100000000) {
+				if (p == 100000000) {
+					r = ammount / (p / 10);
+					if (r >= 11 && r <= 19) {
+						words = words + doubleDigit[r % 10] + " crore ";
+						p = p / 10;
+					} else {
+						words = words + tens[r / 10];
+						words = words + singleDigit[r % 10] + " crore ";
+						p = p / 10;
 					}
-				}
-				else if(p==10000000) {
-					words=words+singleDigit[r] + " crore ";
+				} else if (p == 10000000) {
+					words = words + singleDigit[r] + " crore ";
 				}
 			}
-			//----1,00,000   10,00,000----------
-			else if(p==100000 || p==1000000) {
-				if(p==1000000) {
-					r = ammount / (p/10);
-					if(r>=11 && r<=19) {
-						words=words+doubleDigit[r%10] + " Lakh ";
-						p=p/10;
-					}else {
-						words=words+tens[r/10];
-						words=words+singleDigit[r%10] + " Lakh ";
-						p=p/10;
+			// 1 Lakh To 10 Lakh
+			else if (p == 100000 || p == 1000000) {
+				if (p == 1000000) {
+					r = ammount / (p / 10);
+					if (r >= 11 && r <= 19) {
+						words = words + doubleDigit[r % 10] + " Lakh ";
+						p = p / 10;
+					} else {
+						words = words + tens[r / 10];
+						words = words + singleDigit[r % 10] + " Lakh ";
+						p = p / 10;
 					}
-				}
-				else if(p==100000) {
-					words=words+singleDigit[r] + " Lakh ";
+				} else if (p == 100000) {
+					words = words + singleDigit[r] + " Lakh ";
 				}
 			}
-			//---------1,000  10,000-------------
-			else if(p==1000 || p==10000) {
-				if(p==10000) {
-					r = ammount / (p/10);
-					if(r>=11 && r<=19) {
-						words=words+doubleDigit[r%10] + " Thousand ";
-						p=p/10;
-					}else {
-						words=words+tens[r/10];
-						words=words+singleDigit[r%10] + " Thousand ";
+			// 1 Thousand To 10 Thousand
+			else if (p == 1000 || p == 10000) {
+				if (p == 10000) {
+					r = ammount / (p / 10);
+					if (r >= 11 && r <= 19) {
+						words = words + doubleDigit[r % 10] + " Thousand ";
+						p = p / 10;
+					} else {
+						words = words + tens[r / 10];
+						words = words + singleDigit[r % 10] + " Thousand ";
 
-						p=p/10;
+						p = p / 10;
 					}
-				}
-				else if(p==1000) {
-					words=words+singleDigit[r] + " Thousand ";
+				} else if (p == 1000) {
+					words = words + singleDigit[r] + " Thousand ";
 
 				}
 			}
-			//-----------100-----------
+			// Hundred
 			else if (p == 100) {
-				if(ammount/100!=0) words += singleDigit[r] + " hundred ";
+				if (ammount / 100 != 0)
+					words += singleDigit[r] + " hundred ";
 			} else if ((p == 10) && (ammount >= 11 && ammount <= 19)) {
 				words += doubleDigit[ammount % 10];
 				break;
